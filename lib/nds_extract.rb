5c6125ffb502e6ac91f163b0a4be20d0541f4aca
Patrick A <akolo@hotmail.com>
2020-02-29 19:31:08 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -62,8 +62,8 @@ def movies_with_director_key(name, movies_collection)
   while index < movies_collection.length do 
     test_set = movies_collection
     director_name = name
-    result[:director_name] = director_name
-    result[director_name] = test_set
+    :director_name = director_name
+    result[:director_name] = test_set
     updated_movies[index]  = result
     binding.pry
     index += 1 
